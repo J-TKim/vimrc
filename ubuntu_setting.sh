4b07c8b219c55c971883b67285d981daca930715
Kim, Jeong Tae <68172322+J-TKim@users.noreply.github.com>
2021-09-12 15:24:55 +0900

    Create ubuntu_setting.sh

---
+++ ubuntu_setting.sh
@@ -0,0 +1,8 @@
+cd ~
+git clone https://github.com/J-TKim/vimrc.git
+cp vimrc/py_vimrc_linux .vimrc
+
+mkdir -p ~/.vim/colors
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+sudo apt install exuberant-ctags
